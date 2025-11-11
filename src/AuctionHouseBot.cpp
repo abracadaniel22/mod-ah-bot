a280ec50115efcab33c4499fcbb5e3ced00db572
Abracadaniel22 <7702641+abracadaniel22@users.noreply.github.com>
2025-11-11 21:29:17 +0000

    Improve logging

--- src/AuctionHouseBot.cpp
+++ src/AuctionHouseBot.cpp
@@ -658,7 +658,7 @@ void AuctionHouseBot::addNewAuctions(Player* AHBplayer, AHBConfig *config)
         items = (maxItems - auctions);
 
     if (debug_Out)
-        LOG_ERROR("module", "AHSeller: House ID: {}. Current auction count: {}. Adding {} auctions", items);
+        LOG_ERROR("module", "AHSeller: House ID: {}. Current auction count: {}. Adding {} auctions", config->GetAHID(), auctions, items);
 
     // only insert a few at a time, so as not to peg the processor
     for (uint32 cnt = 1; cnt <= items; cnt++)
